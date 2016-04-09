22114f9da27985c655114bc9a3b9765ccd017e2a
manojrammurthy <manrox.drag@gmail.com>
2016-04-09 10:45:04 +0530

    first commit

--- chatapp/src/main.js
+++
@@ -1,7 +0,0 @@
-$ = Jquery = require('jquery');
-
-var React = require('react');
-var ReactDOM  = require('react-dom');
-var Chat = require('./chatPage');
-
-ReactDOM.render(<Chat />,document.getElementById('app'));
